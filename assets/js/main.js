679f3ec485324d4de248925a130085d67f80dbd1
Yevhen Onoprienko <fromzpcity@gmail.com>
2026-06-13 21:40:41 +0300

    02. usefull-things-477-form. UI. Form.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(".input")
+    .focus(function () {
+        $(this).parent().addClass("focus");
+    })
+    .blur(function () {
+        if ($(this).val() === "") {
+            $(this).parent().removeClass("focus");
+        }
+    });
